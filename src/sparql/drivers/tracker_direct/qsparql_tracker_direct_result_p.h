2388e6ed16d7f401aba0e18b9331f225ec4d18bd
James Thomas <james.thomas@codethink.co.uk>
2011-06-13 13:55:49 +0300

    add base class

---
+++ src/sparql/drivers/tracker_direct/qsparql_tracker_direct_result_p.h
@@ -0,0 +1,123 @@
+/****************************************************************************
+**
+** Copyright (C) 2010-2011 Nokia Corporation and/or its subsidiary(-ies).
+** All rights reserved.
+** Contact: Nokia Corporation (ivan.frade@nokia.com)
+**
+** This file is part of the QtSparql module (not yet part of the Qt Toolkit).
+**
+** $QT_BEGIN_LICENSE:LGPL$
+** GNU Lesser General Public License Usage
+** This file may be used under the terms of the GNU Lesser General Public
+** License version 2.1 as published by the Free Software Foundation and
+** appearing in the file LICENSE.LGPL included in the packaging of this
+** file. Please review the following information to ensure the GNU Lesser
+** General Public License version 2.1 requirements will be met:
+** http://www.gnu.org/licenses/old-licenses/lgpl-2.1.html.
+**
+** In addition, as a special exception, Nokia gives you certain additional
+** rights. These rights are described in the Nokia Qt LGPL Exception
+** version 1.1, included in the file LGPL_EXCEPTION.txt in this package.
+**
+** GNU General Public License Usage
+** Alternatively, this file may be used under the terms of the GNU General
+** Public License version 3.0 as published by the Free Software Foundation
+** and appearing in the file LICENSE.GPL included in the packaging of this
+** file. Please review the following information to ensure the GNU General
+** Public License version 3.0 requirements will be met:
+** http://www.gnu.org/copyleft/gpl.html.
+**
+** Other Usage
+** Alternatively, this file may be used in accordance with the terms and
+** conditions contained in a signed written agreement between you and Nokia.
+**
+** If you have questions regarding the use of this file, please contact
+** Nokia at ivan.frade@nokia.com.
+** $QT_END_LICENSE$
+**
+****************************************************************************/
+
+#ifndef QSPARQL_TRACKER_DIRECT_RESULT_P_H
+#define QSPARQL_TRACKER_DIRECT_RESULT_P_H
+
+#include <QtSparql/qsparqlresult.h>
+#include <QtSparql/qsparqlquery.h>
+#include <QRunnable>
+#include <QThreadPool>
+#include <QtCore/qsemaphore.h>
+#include <QDebug>
+
+#ifdef QT_PLUGIN
+#define Q_EXPORT_SPARQLDRIVER_TRACKER_DIRECT
+#else
+#define Q_EXPORT_SPARQLDRIVER_TRACKER_DIRECT Q_SPARQL_EXPORT
+#endif
+
+QT_BEGIN_HEADER
+
+QT_BEGIN_NAMESPACE
+
+class QTrackerDirectQueryRunner : public QRunnable
+{
+public:
+   // QTrackerDirectFetcherPrivate(QTrackerDirectSelectResult *res) : result(res), runFinished(0), runSemaphore(1) { setAutoDelete(false); }
+
+   QTrackerDirectQueryRunner() { setAutoDelete(false); }
+
+    QAtomicInt runFinished;
+    QSemaphore runSemaphore;
+    bool started;
+
+    void runOrWait()
+    {
+        if(acquireRunSemaphore())
+        {
+            qDebug() << "going to run!";
+            run();
+        }        
+        else
+            wait();
+    }
+
+    void queue(QThreadPool& threadPool)
+    {
+        if(acquireRunSemaphore())
+            threadPool.start(this);
+    }
+
+    void wait()
+    {
+        //if something has has acquired the semaphore (eg the fetcher thread)
+        //this will block until it is released in run
+        qDebug() << "waiting in wait()";
+        runSemaphore.acquire(1);
+        runSemaphore.release(1);
+    }
+
+private:
+
+    void run() {}
+    bool acquireRunSemaphore()
+    {
+        return runSemaphore.tryAcquire(1);
+    }
+
+};
+
+
+class Q_EXPORT_SPARQLDRIVER_TRACKER_DIRECT QTrackerDirectResult : public QSparqlResult
+{
+    Q_OBJECT
+public:
+
+   virtual void stopAndWait();
+   QTrackerDirectQueryRunner *queryRunner;
+  // virtual void runQuery() const;
+
+};
+
+QT_END_NAMESPACE
+
+QT_END_HEADER
+
+#endif // QSPARQL_TRACKER_DIRECT_RESULT_P_H
