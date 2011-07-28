a891252a86f1e50b45fdce9dbcccdbc16b3566b1
James Thomas <james.thomas@codethink.co.uk>
2011-07-28 11:58:15 +0300

    Remove "unstable" from documentation
    Remove styling from class table

--- src/sparql/kernel/doc.cpp
+++ src/sparql/kernel/doc.cpp
@@ -1,8 +1,6 @@
 /*!
     \mainpage The QtSparql library
 
-    \brief <center>unstable</center>
-
     \section Introduction
 
     <b>Description</b>
@@ -31,42 +29,62 @@
       href="http://docs.openlinksw.com/virtuoso/">Virtuoso</a>
 
     <b>List of classes QtSparql API provides:</b>
-    <p><table width="100%">
-    <tr valign="top" bgcolor="#ffffff"><td><i>Class</i></td><td><i>Description</i></td></tr>
+    <p>
+    <table>
+    <tr>
+    <th>Class</th>
+    <th>Description</th>
+    </tr>
+    <tr>
+    <td><b>QSparqlConnection</b></td>
+    <td>Interface for accessing an RDF store.</td>
+    </tr>
 
-    <tr valign="top" bgcolor="#f0f0f0"><td><b>QSparqlConnection</b></td>
-    <td>Interface for accessing an RDF store.</td></tr>
+    <tr>
+    <td><b>QSparqlConnectionOptions</b></td>
+    <td>Encapsulates options given to QSparqlConnection. Some options are used only by
+    some drivers.</td>
+    </tr>
 
-    <tr valign="top" bgcolor="#f0f0f0"><td><b>QSparqlConnectionOptions</b></td>
-    <td>Encapsulates options given to QSparqlConnection. Some options are used
-    only by some drivers.</td></tr>
+    <tr>
+    <td><b>QSparqlError</b></td>
+    <td>SPARQL error information.</td>
+    </tr>
 
-    <tr valign="top" bgcolor="#f0f0f0"><td><b>QSparqlError</b></td><td>SPARQL
-    error information.</td></tr>
+    <tr>
+    <td><b>QSparqlBinding</b></td>
+    <td>Handles a binding between a SPARQL query variable name and the value of the RDF
+    node.</td>
+    </tr>
 
-    <tr valign="top" bgcolor="#f0f0f0"><td><b>QSparqlBinding</b></td><td>Handles
-    a binding between a SPARQL query variable name and the value of the RDF
-    node.</td></tr>
+    <tr>
+    <td><b>QSparqlQuery</b></td>
+    <td>Means of executing and manipulating SPARQL statements.</td>
+    </tr>
 
-    <tr valign="top" bgcolor="#f0f0f0"><td><b>QSparqlQuery</b></td><td>Means of
-    executing and manipulating SPARQL statements.</td></tr>
+    <tr>
+    <td><b>QSparqlQueryOptions</b></td>
+    <td>Encapsulates query execution options given to QSparqlConnection::exec(const  QSparqlQuery&,
+    const QSparqlQueryOptions&) Some options are used only by some drivers.</td>
+    </tr>
 
-    <tr valign="top" bgcolor="#f0f0f0"><td><b>QSparqlQueryOptions</b></td>
-    <td>Encapsulates query execution options given to QSparqlConnection::exec(const  QSparqlQuery&, const QSparqlQueryOptions&)
-    Some options are used only by some drivers.</td></tr>
+    <tr>
+    <td><b>QSparqlQueryModel</b></td>
+    <td>Read-only data model for SPARQL result sets.</td>
+    </tr>
 
-    <tr valign="top"
-    bgcolor="#f0f0f0"><td><b>QSparqlQueryModel</b></td><td>Read-only data model
-    for SPARQL result sets.</td></tr>
+    <tr>
+    <td><b>QSparqlResultRow</b></td>
+    <td>Encapsulates a row in the results of a query.</td>
+    </tr>
 
-    <tr valign="top"
-    bgcolor="#f0f0f0"><td><b>QSparqlResultRow</b></td><td>Encapsulates a row in
-    the results of a query.</td></tr>
+    <tr>
+    <td><b>QSparqlResult</b></td>
+    <td>Abstract interface for accessing the results of an executed QSparqlQuery.</td>
+    </tr>
 
-    <tr valign="top" bgcolor="#f0f0f0"><td><b>QSparqlResult</b></td><td>Abstract
-    interface for accessing the results of an executed QSparqlQuery.</td></tr>
-
-    </table></p>
+    </table>
+    </p>
 
     \attention The QtSparql library is not yet stable; we make no
     promises about API / ABI compatibility!
